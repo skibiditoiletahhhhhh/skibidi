22135c30560f649531ac467d8b0d4cc0fc2aafe4
skibidii <giahuydang78@gmail.com>
2025-10-22 17:54:48 +0700

    Fix ngrok command by adding './' prefix

--- a.sh
+++ a.sh
@@ -1,4 +1,4 @@
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
 tar -xf ngrok*
-ngrok config add-authtoken 2eaehnkTjyL4OmHHKgaCsGmTBEU_6QCyvFZNhdNgHQgNbNQ1g
-ngrok tcp 22
+./ngrok config add-authtoken 2eaehnkTjyL4OmHHKgaCsGmTBEU_6QCyvFZNhdNgHQgNbNQ1g
+./ngrok tcp 22
